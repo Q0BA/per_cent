c91079d82d263b0515793efc7d710bcbde5dd0d6
Qoba <reshetovandrew1@gmail.com>
2022-04-12 19:56:04 +0300

    Update per_cent.py

--- per_cent.py
+++ per_cent.py
@@ -1,6 +1,14 @@
 per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
 
-money = input('Введите сумму зачисляемую на счёт: ')
+money = int(input('Введите сумму зачисляемую на счёт: '))
 
 per_cent_list = list(map(float, per_cent.values()))
-print(per_cent_list)
+
+bank1 = money*per_cent_list[0]/100
+bank2 = money*per_cent_list[1]/100
+bank3 = money*per_cent_list[2]/100
+bank4 = money*per_cent_list[3]/100
+
+deposit = [bank1,bank2,bank3,bank4]
+
+print("Максимальная сумма, которую вы можете заработать — ",max(deposit))
