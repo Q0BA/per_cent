fc12f90b8e9f949512af68366db6e8b18cf504c8
Qoba <reshetovandrew1@gmail.com>
2022-04-12 16:18:23 +0300

    Update per_cent.py

--- per_cent.py
+++ per_cent.py
@@ -2,6 +2,5 @@ per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
 
 money = input('Введите сумму зачисляемую на счёт: ')
 
-per_cent_list = per_cent.values()
-
-print(max(per_cent_list))
+per_cent_list = list(map(float, per_cent.values()))
+print(per_cent_list)
